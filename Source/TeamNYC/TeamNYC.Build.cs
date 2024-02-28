35dca5766b27b7df24f45b9be68220144e48a44c
Tadminster <91bottlenine@naver.com>
2024-02-28 19:55:54 +0900

    1. "TeamNYC.Build.cs" - PublicDependencyModuleNames 배열에 새로운 종속성인 EnhancedInput, AIModule, GameplayTasks가 포함되도록 변경. - PublicIncludePaths가 ModuleDirectory를 포함하도록 업데이트.
    2. "TeamNYC.uproject"
    - AdditionalDependencies 배열에 "Engine"을 포함하도록 업데이트
    
    3. "CharacterPrototype.cpp/h"
    - 플레이어와 몬스터의 조상 클래스 추가

--- Source/TeamNYC/TeamNYC.Build.cs
+++ Source/TeamNYC/TeamNYC.Build.cs
@@ -7,17 +7,27 @@ public class TeamNYC : ModuleRules
 	public TeamNYC(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.AddRange(new string[]
+		{   "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "AIModule",
+            "GameplayTasks"
+		});
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        PublicIncludePaths.Add(ModuleDirectory);
+
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
