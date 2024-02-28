3e6d22e04463cff2ef2ce441213983418b1c510a
Tadminster <91bottlenine@naver.com>
2024-02-29 01:25:52 +0900

    TeamNYC.uproject에 새로운 종속성 및 플러그인 추가, KeyboardPlayerController 클래스 생성
    1. TeamNYC.uproject에 새로운 종속성 EnhancedInput, UMG, AIModule이 포함.
    
    2. 입력 컴포넌트 설정, 플레이어 이동, 점프 동작 처리를 위한 메서드를 포함 AKeyboardPlayerController 클래스 생성. 차후에 다른 입력 처리도 추가 할 예정.

---
+++ Source/TeamNYC/Character/Player/KeyboardPlayerController.cpp
@@ -0,0 +1,158 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Player/KeyboardPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputAction.h"
+#include "InputActionValue.h"
+#include "InputMappingContext.h"
+
+#include "GameFramework/Character.h"
+
+
+#include "Character/Player/PlayerCharacter.h"
+
+AKeyboardPlayerController::AKeyboardPlayerController()
+{
+	bShowMouseCursor = true;		// Show the mouse cursor
+	bEnableClickEvents = true;		// Enable mouse click events
+	bEnableMouseOverEvents = true;	// Enable mouse over events
+
+	// Set DMC
+	FString DmcPath = TEXT("/Script/EnhancedInput.InputMappingContext'/Game/Assets/Input/IMC_Player.IMC_Player'");
+	UInputMappingContext* DmcObj = Cast<UInputMappingContext>(StaticLoadObject(UInputMappingContext::StaticClass(), nullptr, *DmcPath));
+	if (DmcObj) InputMappingContext = DmcObj;
+	else UE_LOG(LogTemp, Warning, TEXT("Failed to load DMC: %s"), *DmcPath);
+
+	// Set IA
+	// MoveAction
+	FString IaPath = TEXT("/Script/EnhancedInput.InputAction'/Game/Assets/Input/IA_Move.IA_Move'");
+	UInputAction* IaMove = Cast<UInputAction>(StaticLoadObject(UInputAction::StaticClass(), nullptr, *IaPath));
+	if (IaMove) MoveAction = IaMove;
+	else UE_LOG(LogTemp, Warning, TEXT("Failed to load IA_Move: %s"), *IaPath);
+
+	// JumpAction
+	IaPath = TEXT("/Script/EnhancedInput.InputAction'/Game/Assets/Input/IA_Jump.IA_Jump'");
+	UInputAction* IaJump = Cast<UInputAction>(StaticLoadObject(UInputAction::StaticClass(), nullptr, *IaPath));
+	if (IaJump) JumpAction = IaJump;
+	else UE_LOG(LogTemp, Warning, TEXT("Failed to load IA_Jump: %s"), *IaPath);
+
+}
+
+void AKeyboardPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Set input mode
+	SetInputMode(FInputModeGameAndUI());
+
+	// Get PlayerPawn->GetController()
+	OwnerPawn = Cast<APawn>(GetPawn());
+	if (OwnerPawn)
+	{
+		UE_LOG(LogTemp, Display, TEXT("OwnerPawn: %s"), *OwnerPawn->GetName());
+
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("OwnerPawn is NULL"));
+	}
+
+	// Set Controller
+	Controller = Cast<APlayerController>(this);
+	if (Controller)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Controller: %s"), *Controller->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Controller is NULL"));
+	}
+
+
+	// Bind input
+	//Add Input Mapping Context
+	
+	if (Controller)
+	{
+		if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+		{
+			if (UEnhancedInputLocalPlayerSubsystem* Subsystem
+				= ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+			{
+				Subsystem->AddMappingContext(InputMappingContext, 0);
+			}
+		}
+	}
+}
+
+void AKeyboardPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if (EnhancedInputComponent)
+	{
+		// Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AKeyboardPlayerController::Move);
+
+		// Jumping
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &AKeyboardPlayerController::StartJump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &AKeyboardPlayerController::StopJump);
+	}
+}
+
+void AKeyboardPlayerController::Move(const FInputActionValue& Value)
+{
+	if (!OwnerPawn)
+	{
+		UE_LOG(LogTemp, Error, TEXT("OwnerPawn is NULL, cannot move."));
+		return;
+	}
+
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
+	//UE_LOG(LogTemp, Warning, TEXT("Move called: X=%f, Y=%f"), MovementVector.X, MovementVector.Y);
+
+	if (Controller)
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get camera forward vector
+		const FVector ForwardDirection = OwnerPawn->GetActorForwardVector();
+		const FVector RightDirection = OwnerPawn->GetActorRightVector();
+		//const FVector RightDirection = OwnerPawn->Camera->GetRightVector();
+
+		// add movement 
+		OwnerPawn->AddMovementInput(ForwardDirection, MovementVector.Y);
+		OwnerPawn->AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void AKeyboardPlayerController::StartJump(const FInputActionValue& Value)
+{
+	if (OwnerPawn)
+	{
+		ACharacter* CharacterTemp = Cast<ACharacter>(OwnerPawn);
+		if (CharacterTemp)
+		{
+			CharacterTemp->Jump();
+		}
+	}
+}
+
+void AKeyboardPlayerController::StopJump(const FInputActionValue& Value)
+{
+	if (OwnerPawn)
+	{
+		ACharacter* CharacterTemp = Cast<ACharacter>(OwnerPawn);
+		if (CharacterTemp)
+		{
+			CharacterTemp->StopJumping();
+		}
+	}
+}
---
+++ Source/TeamNYC/Character/Player/KeyboardPlayerController.h
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "KeyboardPlayerController.generated.h"
+
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+/**********************************************
+ * WASD 움직임을 제공하는 플레이어 컨트롤러
+ **********************************************/
+UCLASS()
+class TEAMNYC_API AKeyboardPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+private:
+	// OwnerPawn
+	UPROPERTY(VisibleAnywhere, Category = "Input")
+	APawn* OwnerPawn{ nullptr };
+
+	// Controller
+	UPROPERTY(VisibleAnywhere, Category = "Input")
+	AController* Controller{ nullptr };
+
+	// MappingContext
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputMappingContext* InputMappingContext;
+
+	// Move IA
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MoveAction;
+
+	// Jump IA
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* JumpAction;
+
+	// Look IA
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* LookAction;
+
+	// WalkSlow IA
+	//UPROPERTY(EditAnywhere, Category = "Input")
+	//UInputAction* WalkSlowAction;
+
+	// WalkFast IA
+	//UPROPERTY(EditAnywhere, Category = "Input")
+	//UInputAction* WalkFastAction;
+
+	// Attack IA
+	//UPROPERTY(EditAnywhere, Category = "Input")
+	//UInputAction* AttackAction;
+
+public:
+	AKeyboardPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void SetupInputComponent() override;
+
+	void Move(const FInputActionValue& Value);
+	//void Look(const FInputActionValue& Value);
+	void StartJump(const FInputActionValue& Value);
+	void StopJump(const FInputActionValue& Value);
+};
