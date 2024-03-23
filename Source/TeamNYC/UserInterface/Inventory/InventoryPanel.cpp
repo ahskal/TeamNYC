c2abf50bcfd8d4c3c6e87c39acb93f9071b6e8d0
ahskal <kkj380377@gmail.com>
2024-03-23 23:14:29 +0900

    인벤토리 패널 클레스 내용추가

--- Source/TeamNYC/UserInterface/Inventory/InventoryPanel.cpp
+++ Source/TeamNYC/UserInterface/Inventory/InventoryPanel.cpp
@@ -1,5 +1,61 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "Character/Player/PlayerCharacter.h"
 #include "UserInterface/Inventory/InventoryPanel.h"
+#include "UserInterface/Inventory/InventoryItemSlot.h"
+#include "UserInterface/Inventory/ItemDragDropOperation.h"
 
+#include "Components/InventoryComponent.h"
+
+// engine
+#include "Components/TextBlock.h"
+#include "Components/WrapBox.h"
+
+void UInventoryPanel::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	PlayerCharacter = Cast<APlayerCharacter>(GetOwningPlayerPawn());
+	if (PlayerCharacter)
+	{
+		InventoryReference = PlayerCharacter->GetInventory();
+		if (InventoryReference)
+		{
+			InventoryReference->OnInventoryUpdated.AddUObject(this, &UInventoryPanel::RefreshInventory);
+			SetInfoText();
+		}
+	}
+}
+
+void UInventoryPanel::SetInfoText() const
+{
+
+	WeightInfo->SetText(FText::Format(FText::FromString("{0}/{1}"),
+		InventoryReference->GetInventoryTotalWeight(),
+		InventoryReference->GetWeightCapacity()));
+	CapacityInfo->SetText(FText::Format(FText::FromString("{0}/{1}"),
+		InventoryReference->GetInventoryContents().Num(),
+		InventoryReference->GetWeightCapacity()));
+}
+
+void UInventoryPanel::RefreshInventory()
+{
+	if (InventoryReference && InventorySlotClass)
+	{
+		InventoryUWrapBox->ClearChildren();
+
+		for (UItemBase* const& InventoryItem : InventoryReference->GetInventoryContents())
+		{
+			UInventoryItemSlot* ItemSlot = CreateWidget<UInventoryItemSlot>(this, InventorySlotClass);
+
+			//ItemSlot->SetItemReference(InventoryItem);
+			InventoryUWrapBox->AddChildToWrapBox(ItemSlot);
+		}
+		SetInfoText();
+	}
+}
+
+bool UInventoryPanel::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEnvet, UDragDropOperation* InOperation)
+{
+	return NativeOnDrop(InGeometry, InDragDropEnvet, InOperation);
+}
